03f87ac7b6af890c114fcdf9db8318693ae68eb7
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-16 06:17:39 -0800

    Setup VS Code IDE with Prettier, ESlint, and Nodemon

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // use express static with the directory
-app.use(express.static(path.join(__dirname, './static')))
+app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (request,response) => {
-    //response.send('Hello Express :)')
-    response.sendFile(path.join(__dirname, 'static/index.html'))
-})
+app.get('/', (request, response) => {
+  //response.send('Hello Express :)')
+  response.sendFile(path.join(__dirname, 'static/index.html'));
+});
 
 app.get('/speakers', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/speakers.html'))
-})
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Express Server listening on port ${port}`)
-})
+  console.log(`Express Server listening on port ${port}`);
+});
