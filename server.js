35f833579f9ce0d6cf06739678c0e0a1885dfbb1
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-17 07:09:21 -0800

    Move static/index.html to views/pages/index.ejs and pass pageTitle param

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ app.set('views', path.join(__dirname, './views'));
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-  //response.send('Hello Express :)')
-  response.sendFile(path.join(__dirname, 'static/index.html'));
+  // response.send('Hello Express :)')
+  // response.sendFile(path.join(__dirname, 'static/index.html'));
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/speakers', (request, response) => {
