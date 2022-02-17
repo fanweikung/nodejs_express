8f94e2f6ba27bcc84edaa83235388833bacc0588
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-02-17 10:31:49 -0800

    Add dedicated routing infrastrusture with / route to routes/index.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes'); // will default to ./routes/index.js
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-// use express static with the directory
+// use express static middleware with the directory
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (request, response) => {
-  // response.send('Hello Express :)')
-  // response.sendFile(path.join(__dirname, 'static/index.html'));
-  response.render('pages/index', { pageTitle: 'Welcome' });
-});
+// use another route to catch all; need to call the routes() function to get the router object back
+app.use('/', routes());
 
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+// app.get('/', (request, response) => {
+//   // response.send('Hello Express :)')
+//   // response.sendFile(path.join(__dirname, 'static/index.html'));
+//   response.render('pages/index', { pageTitle: 'Welcome' });
+// });
+
+// app.get('/speakers', (request, response) => {
+//   response.sendFile(path.join(__dirname, './static/speakers.html'));
+// });
 
 app.listen(port, () => {
   console.log(`Express Server listening on port ${port}`);
